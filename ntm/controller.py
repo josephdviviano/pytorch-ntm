465df773ce350d35ce500e1bd5e5751fb86a1ce7
Joseph Viviano <vivianoj@leto52.iro.umontreal.ca>
2018-03-26 17:12:51 -0400

    added mlp controller

--- ntm/controller.py
+++ ntm/controller.py
@@ -45,3 +45,39 @@ class LSTMController(nn.Module):
         x = x.unsqueeze(0)
         outp, state = self.lstm(x, prev_state)
         return outp.squeeze(0), state
+
+class FFController(nn.Module):
+    """
+    An NTM controller based on MLP. Also see,
+    https://github.com/DavideA/ntm-copy/blob/master/controller.py
+    """
+    def __init__(self, num_inputs, num_outputs, num_layers):
+        super(FFController, self).__init__()
+
+        self.num_inputs = num_inputs
+        self.num_outputs = num_outputs
+        self.num_layers = 1 # TODO: num_layers hard coded to 1 for now
+
+        self.mlp = nn.Sequential([
+            nn.Linear(num_inputs, num_outputs),
+            nn.ReLU()])
+
+        self.reset_parameters()
+
+    #def create_new_state(self, batch_size):
+    #    # Dimension: (num_layers * num_directions, batch, hidden_size)
+    #    lstm_h = self.lstm_h_bias.clone().repeat(1, batch_size, 1)
+    #    lstm_c = self.lstm_c_bias.clone().repeat(1, batch_size, 1)
+    #    return lstm_h, lstm_c
+
+    def reset_parameters(self):
+        for k, v in self.mlp.named_parameters():
+            if k.endswith('weight'):
+                torch.nn.init.xavier_uniform(v, gain=calculate_gain('relu'))
+
+    def size(self):
+        return self.num_inputs, self.num_outputs
+
+    def forward(self, x, prev_state):
+        return(self.mlp(X).squeeze())
+
